2f93f34c042cdf83ceedae3f6a8d08f320383891
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-10-25 15:56:40 -0300

    Correções

--- campo_minado/main.c
+++ campo_minado/main.c
@@ -14,9 +14,14 @@ const int comprimento = 640;
 const int altura = 640;
 bool loop = true;
 
+//  Matriz do campo minado
+int M[10][10];
+
 //  Funcoes
-//  Funcao responsavel pela tela do jogo
 void graficos(void);
+void logica(void);
+void desenhar_campo_minado(void);
+int preencher_matriz(void);
 
 //  Variaveis SDL
 SDL_Window *janela = NULL;
@@ -50,6 +55,9 @@ int main()
             }
         }
 
+        //  Logica
+        logica();
+
         //  Graficos
         graficos();
     }
@@ -61,6 +69,12 @@ int main()
     return 0;
 }
 
+//  Funcao responsavel pela logica do jogo
+void logica(void)
+{
+    preencher_matriz();
+}
+
 //  Funcao responsavel pela tela do jogo
 void graficos(void)
 {
@@ -68,6 +82,70 @@ void graficos(void)
     SDL_SetRenderDrawColor(tela, 255, 255, 255, 255);
     SDL_RenderClear(tela);
 
+    //  Desenhando campo minado
+    desenhar_campo_minado();
+
     //  Atualizando a tela
     SDL_RenderPresent(tela);
+}
+
+//  Funcao que desenha o campo minado
+void desenhar_campo_minado(void)
+{
+    int linha, coluna, quadrado_x, quadrado_y, quadrado_comp = (comprimento / 10), quadrado_alt = (altura / 10);
+    for(linha = 0; linha < 10; linha++)
+    {
+        for(coluna = 0; coluna < 10; coluna++)
+        {
+            //  Definindo cor do quadrado
+            if((linha + coluna) % 2 == 0)
+            {
+                //  Definindo cor branca para uma parte dos quadrados
+                SDL_SetRenderDrawColor(tela, 0, 0, 0, 0);
+            }
+            else
+            {
+                //  Definindo cor azul (com verde) para os quadrados restantes
+                SDL_SetRenderDrawColor(tela, 255, 255, 255, 255);
+            }
+            //  Calculo de onde iniciara cada quadrado na tela
+            quadrado_x = coluna * quadrado_comp;
+            quadrado_y = linha * quadrado_alt;
+            //  Criando de fato os quadrados
+            SDL_Rect quadrado = {quadrado_x, quadrado_y, quadrado_comp, quadrado_alt};
+            //  Pintando de fato o quadrado na tela
+            SDL_RenderFillRect(tela, &quadrado);
+        }
+    }
+}
+
+//  Funcao que preenche a matriz do campo minado
+int preencher_matriz(void)
+{
+    int linha, coluna, bombas = 0;
+    for(linha = 0; linha < 10; linha++)
+    {
+        for(coluna = 0; coluna < 10; coluna++)
+        {
+            if((linha == 0 || linha == 9) || (coluna == 0 || coluna == 9))
+            {
+                M[linha][coluna] = -1;
+            }
+            else
+            {
+                if((rand() % 10) % 2 == 0)
+                {
+                    M[linha][coluna] = 9;
+                    bombas++;
+                }
+                else
+                {
+                    M[linha][coluna] = 0;
+                }
+
+            }
+
+        }
+    }
+    return bombas;
 }
