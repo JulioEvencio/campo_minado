914e8364991514b541d08720a6e720cb68facaf2
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-10-25 20:55:02 -0300

    Adicionada função para carregar as imagens

--- campo_minado/main.c
+++ campo_minado/main.c
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>>
+#include <SDL2/SDL_image.h>
 
 //  Variaveis
 const int comprimento = 640;
@@ -82,7 +82,55 @@ int main()
 //  Funcao que cria texturas das imagens e as colocam em suas devidas variaveis
 void carregar_imagem(void)
 {
-    //  Code
+    //  Numero zero
+    imagem = IMG_Load("imagem/zero.png");
+    zero = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero um
+    imagem = IMG_Load("imagem/um.png");
+    um = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero dois
+    imagem = IMG_Load("imagem/dois.png");
+    dois = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero tres
+    imagem = IMG_Load("imagem/tres.png");
+    tres = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero quatro
+    imagem = IMG_Load("imagem/quatro.png");
+    quatro = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero cinco
+    imagem = IMG_Load("imagem/cinco.png");
+    cinco = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero seis
+    imagem = IMG_Load("imagem/seis.png");
+    seis = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero sete
+    imagem = IMG_Load("imagem/sete.png");
+    sete = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Numero oito
+    imagem = IMG_Load("imagem/oito.png");
+    oito = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
+
+    //  Bomba
+    imagem = IMG_Load("imagem/mina.png");
+    bomba = SDL_CreateTextureFromSurface(tela, imagem);
+    SDL_FreeSurface(imagem);
 }
 
 //  Funcao responsavel pela logica do programa
@@ -97,6 +145,10 @@ void graficos(void)
     //  Definindo plano de fundo da janela
     SDL_SetRenderDrawColor(tela, 0, 0, 0, 0);
     SDL_RenderClear(tela);
+
+    //  Pintando o zero na tela
+    SDL_Rect zero_img = {50, 50, 100, 100};
+    SDL_RenderCopy(tela, cinco, NULL, &zero_img);
 
     //  Atualizando tela
     SDL_RenderPresent(tela);
