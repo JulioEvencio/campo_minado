c88a765511191c3fafd676bd4865ea9c2f251a6a
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-10-25 20:44:18 -0300

    Refeito código fonte do zero

--- campo_minado/main.c
+++ campo_minado/main.c
@@ -1,34 +1,28 @@
 /*
     Campo minado
 */
-//  Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <time.h>
+#include <SDL2/SDL_image.h>>
 
 //  Variaveis
 const int comprimento = 640;
 const int altura = 640;
 bool loop = true;
 
-//  Matriz do campo minado
-int M[10][10];
-
 //  Funcoes
-void graficos(void);
-void logica(void);
-void desenhar_campo_minado(void);
 void carregar_imagem(void);
-int preencher_matriz(void);
-void colocar_imagens_na_matriz(int bomba, int x, int y, int comp, int alt);
+void logica(void);
+void graficos(void);
 
-//  Variaveis SDL
+//  Variaveis SDl
 SDL_Window *janela = NULL;
 SDL_Renderer *tela = NULL;
 SDL_Surface *imagem = NULL;
+//  Variaveis das imagens
 SDL_Texture *zero = NULL;
 SDL_Texture *um = NULL;
 SDL_Texture *dois = NULL;
@@ -38,7 +32,7 @@ SDL_Texture *cinco = NULL;
 SDL_Texture *seis = NULL;
 SDL_Texture *sete = NULL;
 SDL_Texture *oito = NULL;
-SDL_Texture *mina = NULL;
+SDL_Texture *bomba = NULL;
 
 SDL_Event evento;
 
@@ -49,10 +43,11 @@ int main()
     IMG_Init(IMG_INIT_PNG);
 
     //  Criando janela e tela
-    janela = SDL_CreateWindow("Campo minado", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, comprimento, altura, 0);
+    janela = SDL_CreateWindow("Campo Minado", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, comprimento, altura, 0);
     tela = SDL_CreateRenderer(janela, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    //  Funcao que vai carregar imagens
+    //  Carregar imagens em suas variaveis
+    carregar_imagem();
 
     //  Loop do jogo
     while(loop)
@@ -60,7 +55,8 @@ int main()
         //  Verificando se algum evento ocorreu
         while(SDL_PollEvent(&evento) != 0)
         {
-            //  Verificando se o X da janela foi clicado
+            //  Verificando qual evento ocorreu
+            //  Click no X da janela
             if(evento.type == SDL_QUIT)
             {
                 loop = false;
@@ -74,203 +70,34 @@ int main()
         graficos();
     }
 
-    //  Encerrando SDL e SDL IMAGE
+    //  Finalizando SDL e SDL IMAGE
+    SDL_DestroyRenderer(tela);
+    SDL_DestroyWindow(janela);
     IMG_Quit();
     SDL_Quit();
 
     return 0;
 }
 
-//  Funcao responsavel pela logica do jogo
-void logica(void)
-{
-    preencher_matriz();
-}
-
-//  Funcao responsavel pela tela do jogo
-void graficos(void)
-{
-    //  Definindo cor preta para o fundo
-    SDL_SetRenderDrawColor(tela, 255, 255, 255, 255);
-    SDL_RenderClear(tela);
-
-    //  Desenhando campo minado
-    desenhar_campo_minado();
-
-    //  Atualizando a tela
-    SDL_RenderPresent(tela);
-}
-
-//  Funcao que desenha o campo minado
-void desenhar_campo_minado(void)
-{
-    int linha, coluna, quadrado_x, quadrado_y, quadrado_comp = (comprimento / 10), quadrado_alt = (altura / 10);
-    for(linha = 0; linha < 10; linha++)
-    {
-        for(coluna = 0; coluna < 10; coluna++)
-        {
-            //  Definindo cor do quadrado
-            SDL_SetRenderDrawColor(tela, 255, 255, 255, 255);
-            //  Calculo de onde iniciara cada quadrado na tela
-            quadrado_x = coluna * quadrado_comp;
-            quadrado_y = linha * quadrado_alt;
-            //  Criando de fato os quadrados
-            SDL_Rect quadrado = {quadrado_x, quadrado_y, quadrado_comp, quadrado_alt};
-            //  Pintando de fato o quadrado na tela
-            SDL_RenderFillRect(tela, &quadrado);
-            //  Funcao que coloca as imagens no campo minado
-            colocar_imagens_na_matriz(M[linha][coluna], quadrado_x, quadrado_y, quadrado_comp, quadrado_y);
-        }
-    }
-}
-
-//  Funcao que pega as imagens dos arquivos e as colocam em variaveis usaveis para o programa
+//  Funcao que cria texturas das imagens e as colocam em suas devidas variaveis
 void carregar_imagem(void)
 {
-    //  Zero
-    imagem = IMG_Load("imagem/zero.png");
-    zero = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Um
-    imagem = IMG_Load("imagem/um.png");
-    um = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Dois
-    imagem = IMG_Load("imagem/dois.png");
-    dois = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Tres
-    imagem = IMG_Load("imagem/tres.png");
-    tres = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Quatro
-    imagem = IMG_Load("imagem/quatro.png");
-    quatro = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Cinco
-    imagem = IMG_Load("imagem/cinco.png");
-    cinco = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Seis
-    imagem = IMG_Load("imagem/seis.png");
-    seis = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Sete
-    imagem = IMG_Load("imagem/sete.png");
-    sete = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
-
-    //  Oito
-    imagem = IMG_Load("imagem/oito.png");
-    oito = SDL_CreateTextureFromSurface(tela, imagem);
-    SDL_FreeSurface(imagem);
+    //  Code
 }
 
-//  Funcao que preenche a matriz do campo minado
-int preencher_matriz(void)
+//  Funcao responsavel pela logica do programa
+void logica(void)
 {
-    int linha, coluna, bombas = 0;
-    for(linha = 0; linha < 10; linha++)
-    {
-        for(coluna = 0; coluna < 10; coluna++)
-        {
-            if((linha == 0 || linha == 9) || (coluna == 0 || coluna == 9))
-            {
-                M[linha][coluna] = -1;
-            }
-            else
-            {
-                if((rand() % 10) % 2 == 0)
-                {
-                    M[linha][coluna] = 9;
-                    bombas++;
-                }
-                else
-                {
-                    M[linha][coluna] = 0;
-                }
-
-            }
-
-        }
-    }
-    return bombas;
+    //  Code
 }
 
-//  Pintando imagens no campo minado
-void colocar_imagens_na_matriz(int bomba, int x, int y, int comp, int alt)
+//  Funcao responsavel pelos graficos do programa
+void graficos(void)
 {
-    if(bomba == -1)
-    {
-        //  Vazio
-        SDL_Rect vazio_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, zero, NULL, &vazio_img);
-    }
-    if(bomba == 9)
-    {
-        //  Bomba
-        SDL_Rect bomba_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, mina, NULL, &bomba_img);
-    }
-    if(bomba == 0)
-    {
-        //  Zero
-        SDL_Rect zero_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, zero, NULL, &zero_img);
-    }
-    if(bomba == 1)
-    {
-        //  Um
-        SDL_Rect um_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, um, NULL, &um_img);
-    }
-    if(bomba == 2)
-    {
-        //  Dois
-        SDL_Rect dois_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, dois, NULL, &dois_img);
-    }
-    if(bomba == 3)
-    {
-        //  tres
-        SDL_Rect tres_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, tres, NULL, &tres_img);
-    }
-    if(bomba == 4)
-    {
-        //  Quatro
-        SDL_Rect quatro_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, quatro, NULL, &quatro_img);
-    }
-    if(bomba == 5)
-    {
-        //  Cinco
-        SDL_Rect cinco_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, cinco, NULL, &cinco_img);
-    }
-    if(bomba == 6)
-    {
-        //  Seis
-        SDL_Rect seis_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, seis, NULL, &seis_img);
-    }
-    if(bomba == 7)
-    {
-        //  Sete
-        SDL_Rect sete_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, sete, NULL, &sete_img);
-    }
-    if(bomba == 8)
-    {
-        //  Oito
-        SDL_Rect oito_img = {x, y, comp, alt};
-        SDL_RenderCopy(tela, oito, NULL, &oito_img);
-    }
+    //  Definindo plano de fundo da janela
+    SDL_SetRenderDrawColor(tela, 0, 0, 0, 0);
+    SDL_RenderClear(tela);
+
+    //  Atualizando tela
+    SDL_RenderPresent(tela);
 }
