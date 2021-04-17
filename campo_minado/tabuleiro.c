2d623833b314036197da927b81b0e5ec06d952a5
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-17 02:17:29 -0300

    Função que preenche a matriz foi adicionada

--- campo_minado/tabuleiro.c
+++ campo_minado/tabuleiro.c
@@ -6,6 +6,7 @@
 
 struct Tabuleiro {
     int matriz[MATRIZ_LINHA][MATRIZ_COLUNA];
+    int matriz_auxiliar[MATRIZ_LINHA][MATRIZ_COLUNA];
 };
 
 int criar_tabuleiro(Tabuleiro **tabuleiro) {
@@ -16,10 +17,40 @@ int criar_tabuleiro(Tabuleiro **tabuleiro) {
     return 0;
 }
 
-int liberar_tabuleiro(Tabuleiro **tabuleiro) {
+void liberar_tabuleiro(Tabuleiro **tabuleiro) {
+    free(*tabuleiro);
+}
+
+int preencher_tabuleiro(Tabuleiro **tabuleiro) {
+    int bombas = 0;
+
     if (*tabuleiro == NULL) return -1;
 
-    free(*tabuleiro);
+    for (int linha = 0; linha < MATRIZ_LINHA; linha++) {
 
-    return 0;
+        for (int coluna = 0; coluna < MATRIZ_COLUNA; coluna++) {
+
+            if((linha == 0 || linha == 9) || (coluna == 0 || coluna == 9)) {
+
+                (*tabuleiro)->matriz[linha][coluna] = TABULEIRO_BORDA;
+                (*tabuleiro)->matriz_auxiliar[linha][coluna] = TABULEIRO_BORDA;
+
+            } else {
+
+                (*tabuleiro)->matriz_auxiliar[linha][coluna] = TABULEIRO_LOCAL_VAZIO;
+
+                if((rand() % 10) < 3) {
+                    (*tabuleiro)->matriz[linha][coluna] = TABULEIRO_BOMBA;
+                    bombas++;
+                } else {
+                    (*tabuleiro)->matriz[linha][coluna] = TABULEIRO_SEM_BOMBA;
+                }
+
+            }
+
+        }
+
+    }
+
+    return bombas;
 }
--- campo_minado/tabuleiro.h
+++ campo_minado/tabuleiro.h
@@ -1,9 +1,15 @@
 #ifndef TABULEIRO_H_INCLUDED
 #define TABULEIRO_H_INCLUDED
 
+#define TABULEIRO_BOMBA 9
+#define TABULEIRO_SEM_BOMBA 0
+#define TABULEIRO_LOCAL_VAZIO 11
+#define TABULEIRO_BORDA 10
+
 typedef struct Tabuleiro Tabuleiro;
 
 int criar_tabuleiro(Tabuleiro **tabuleiro);
-int liberar_tabuleiro(Tabuleiro **tabuleiro);
+void liberar_tabuleiro(Tabuleiro **tabuleiro);
+int preencher_tabuleiro(Tabuleiro **tabuleiro);
 
 #endif
