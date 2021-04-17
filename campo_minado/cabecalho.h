ac38e48c670be5e51206f8dc386266fb2fc37a54
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-17 00:53:05 -0300

    Base SDL 2 adicionada

--- campo_minado/cabecalho.h
+++ campo_minado/cabecalho.h
@@ -2,3 +2,16 @@
     Constantes e prototipos de funcoes
     genericas
 */
+#ifndef CABECALHO_H_INCLUDED
+#define CABECALHO_H_INCLUDED
+
+#define JANELA_NOME "Campo Minado"
+#define JANELA_LARGURA 640
+#define JANELA_ALTURA 640
+#define JANELA_DELAY 30
+
+enum Menu {
+    SAIR
+};
+
+#endif
--- campo_minado/main.c
+++ campo_minado/main.c
@@ -7,21 +7,69 @@
 #include "cabecalho.h"
 
 int main(void) {
-    //  Code
+    int loop = 1; // Deixando true o loop
+    SDL_Window *janela = NULL;
+    SDL_Renderer *tela = NULL;
+    SDL_Event evento;
 
-    if (SDLInit(SDL_INIT_VIDEO)) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         puts("Erro ao inicializar SDL 2...");
         return 1;
     }
 
-    if (IMG_Init(IMG_INIT_PNG)) {
+    if (IMG_Init(IMG_INIT_PNG) < 0) {
         puts("Erro ao inicializar SDL IMAGE...");
         SDL_Quit();
         return 1;
     }
 
-    //  Code
+    janela = SDL_CreateWindow(
+        JANELA_NOME,
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        JANELA_LARGURA,
+        JANELA_ALTURA,
+        SDL_WINDOW_SHOWN
+    );
 
+    if (janela == NULL) {
+        puts("Erro ao criar janela...");
+        IMG_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    tela = SDL_CreateRenderer(
+        janela,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+
+    if (tela == NULL) {
+        puts("Erro ao renderizar tela...");
+        SDL_DestroyWindow(janela);
+        IMG_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    while (loop) {
+        while (SDL_PollEvent(&evento) != 0) {
+            if (evento.type == SDL_QUIT) {
+                loop = SAIR;
+            }
+        }
+
+        SDL_SetRenderDrawColor(tela, 155, 155, 155, 255);
+        SDL_RenderClear(tela);
+
+        SDL_RenderPresent(tela);
+
+        SDL_Delay(JANELA_DELAY);
+    }
+
+    SDL_DestroyRenderer(tela);
+    SDL_DestroyWindow(janela);
     IMG_Quit();
     SDL_Quit();
 
