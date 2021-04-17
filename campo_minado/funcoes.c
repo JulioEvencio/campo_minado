1d103b6534fc432e818560c6b207bb3ef420422a
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-17 00:36:20 -0300

    Inicialização da SDL 2 e SDL IMAGE

--- campo_minado/funcoes.c
+++ campo_minado/funcoes.c
@@ -1,3 +1,6 @@
 /*
     Implementacao de funcoes genericas
 */
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "cabecalho.h"
--- campo_minado/main.c
+++ campo_minado/main.c
@@ -2,9 +2,28 @@
     Campo Minado versao 2.0
 */
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "cabecalho.h"
 
 int main(void) {
     //  Code
+
+    if (SDLInit(SDL_INIT_VIDEO)) {
+        puts("Erro ao inicializar SDL 2...");
+        return 1;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG)) {
+        puts("Erro ao inicializar SDL IMAGE...");
+        SDL_Quit();
+        return 1;
+    }
+
+    //  Code
+
+    IMG_Quit();
+    SDL_Quit();
 
     return 0;
 }
